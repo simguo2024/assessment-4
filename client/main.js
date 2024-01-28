ce3c4373319136862c7b51da31e9c5ac2742a377
simguo2024 <152562804+simguo2024@users.noreply.github.com>
2024-01-28 13:46:38 -0500

    update mainjs

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ const complimentBtn = document.querySelector("#complimentButton")
 const fortuneBtn = document.querySelector("#fortuneButton")
 const getTimeBtn = document.querySelector("#getTimeButton")
 const whatToEatBtn = document.querySelector("#whatToEatButton")
+const formRes = document.querySelector("#Form")
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/fortune/")
@@ -36,8 +37,22 @@ const getDish = () => {
     });
 };
 
+const getmultiplefortune = (name, count) => {
+    axios.post("http://localhost:4000/api/getmultiplefortune", { name, count })
+    .then(res => {
+        const data = res.data;
+        alert(data.map(sentense => `${sentense.defaultName}: ${sentense.fortune}`).join('\n'));
+    })
+};
+
 
 complimentBtn.addEventListener('click', getCompliment)
 fortuneBtn.addEventListener('click', getFortune)
 getTimeBtn.addEventListener('click', getTime)
 whatToEatBtn.addEventListener('click', getDish)
+formRes.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const count = document.querySelector("#countInput").value;
+    const name = document.querySelector("#nameInput").value;
+    getmultiplefortune(name, count);
+});
